73155457f8469212041c1299e714e0373ec913e4
Bin Liang <bin.liangmathematicsstudent@gmail.com>
2024-01-14 05:56:12 -0500

    fix a bug in retriever

--- src/thinking_flow_utils/retriever.py
+++ src/thinking_flow_utils/retriever.py
@@ -13,7 +13,7 @@ import json
 def retriever(question, k=4):
     # We have already set the top-k = 4. 
 
-    question = "$$David\textquotesingle s$$ cookies were cost thrice as much as $$Jeremy\textquotesingle s$$ cookies. $$David$$ had only $$$0.50$$ cookies while $$Jeremy$$ had~$\tfrac{3}{5}$~as many $$$0.50$$ cookies as $$$0.10$$ cookies. There were $$60$$ fewer $$$0.10$$ cookies than $$$0.50$$ cookies. How much were $$Jeremy\textquotesingle s$$ cookies worth?"
+    # question = "$$David\textquotesingle s$$ cookies were cost thrice as much as $$Jeremy\textquotesingle s$$ cookies. $$David$$ had only $$$0.50$$ cookies while $$Jeremy$$ had~$\tfrac{3}{5}$~as many $$$0.50$$ cookies as $$$0.10$$ cookies. There were $$60$$ fewer $$$0.10$$ cookies than $$$0.50$$ cookies. How much were $$Jeremy\textquotesingle s$$ cookies worth?"
     url = f"https://retrieverk12.ngrok.io/retrieval/{question}"
     response = requests.request("GET", url)
     
